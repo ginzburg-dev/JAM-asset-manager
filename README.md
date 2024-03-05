669adaad03ff372e35de3d2ee3b12e6f61dd81e6
Dmitrii Ginzburg <ginzburgdd@gmail.com>
2024-03-05 13:57:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JAM-asset-manager
+ JAM Asset Manager. Asset Manager Plugin for Maya for lighting and rendering pipeline
---
+++ scripts/maya_scriptJob_things.py
@@ -0,0 +1,6 @@
+import maya.cmds as cmds
+
+def mayaSceneOpened():
+    print("Scene has been loaded")
+
+job_scene_opened = cmds.scriptJob(runOnce = True, e=('SceneOpened', sceneOpened))

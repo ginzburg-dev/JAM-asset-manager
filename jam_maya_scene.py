3dd2a39875f16fdec71c105e260b257a69646603
Dmitrii Ginzburg <ginzburgdd@gmail.com>
2024-03-10 17:57:44 +0100

    update

--- jam_maya_scene.py
+++ jam_maya_scene.py
@@ -49,6 +49,14 @@ def scene_check_message():
      return result_message
 
 def createRenderScene(name,anim_filename,render_filename,rs_filename):
+     fileCheckState = cmds.file(q=True, modified=True)
+     current_scene_name = cmds.file(q=True, sn=True)
+     if fileCheckState:
+          dialog_message = cmds.confirmDialog(title= "Save Changes", message = 'Save changes to '+current_scene_name+'?', button =['Save',"Don't Save", "Cancel"])
+          if dialog_message == 'Cancel':
+               return
+          if dialog_message == 'Save':
+               cmds.file( save=True)
      anim_ref_name = render_filename.replace(name+'.ma',name+'_check_v01.ma')
      # create render directory
      os.makedirs(os.path.dirname(render_filename), exist_ok=True)
@@ -62,22 +70,41 @@ def createRenderScene(name,anim_filename,render_filename,rs_filename):
      #ref
      cmds.file(anim_ref_name, reference=True, mergeNamespacesOnClash=True, namespace='anim');
 
-def openRenderScene():
-     return
+def openRenderScene(path):
+     fileCheckState = cmds.file(q=True, modified=True)
+     current_scene_name = cmds.file(q=True, sn=True)
+     if fileCheckState:
+          dialog_message = cmds.confirmDialog(title= "Save Changes", message = 'Save changes to '+current_scene_name+'?', button =['Save',"Don't Save", "Cancel"])
+          if dialog_message == 'Cancel':
+               return
+          if dialog_message == 'Save':
+               cmds.file( save=True)
+     cmds.file(path, o=True, force=True)
 
 def updateRenderScene(name,anim_filename,render_filename):
-     anim_ref_name = render_filename.replace(name+'.ma',name+'_check_v01.ma')
-     shutil.copy(anim_filename, anim_ref_name)
-     # open renderScene
-     cmds.file(new=True, force=True, bls=True)
-     cmds.file(render_filename, open=True )
+     fileCheckState = cmds.file(q=True, modified=True)
+     current_scene_name = cmds.file(q=True, sn=True)
+     if fileCheckState:
+          dialog_message = cmds.confirmDialog(title= "Save Changes", message = 'Save changes to '+current_scene_name+'?', button =['Save',"Don't Save", "Cancel"])
+          if dialog_message == 'Cancel':
+               return
+          if dialog_message == 'Save':
+               cmds.file( save=True)
+     if os.path.isfile(render_filename):
+          anim_ref_name = render_filename.replace(name+'.ma',name+'_check_v01.ma')
+          shutil.copy(anim_filename, anim_ref_name)
+          # open renderScene
+          cmds.file(new=True, force=True, bls=True)
+          cmds.file(render_filename, open=True )
+     else:
+          cmds.warning("The render scene is empty. Please create a scene to be able to update it")
 
 def publish_scene():
      check_message = scene_check_message()
      result = False
      if len(cmds.file(q=True, sn=True)) != 0:
           if check_message[0] == 0:
-               cmds.confirmDialog(title= "Warning. Scene wasn't publish", message = check_message[1], button =['OK'])
+               cmds.confirmDialog(title= "Warning: Scene wasn't publish", message = check_message[1], button =['OK'])
           else:
                try:
                     cmds.file(save=True)
